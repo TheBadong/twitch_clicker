a0d7161bc18ffa371eb36727d549800cb6cb001b
TheBadong <gautier.bardon2@gmail.com>
2020-05-23 20:18:16 +0200

    adding already present check and removing useless logs

--- contentScript.js
+++ contentScript.js
@@ -1,11 +1,20 @@
 window.onload = () => {
-    console.log('DOM fully loaded and parsed');
+    console.log("Twtich Clicker active !")
     let targetNode = document.querySelector(".community-points-summary")
-    console.log(targetNode)
     const config = {attributes: false, childList: true, subtree: true}
     
+    // Check for already present button
+    if(document.querySelector(".community-points-summary button.tw-button")){
+        document.querySelector(".community-points-summary button.tw-button").click()
+    }
+
+    // Create an observer instance linked to the callback function
+    const observer = new MutationObserver(callback);
+    
+    // Start observing the target node for configured mutations
+    observer.observe(targetNode, config);
+    
     const callback = function(mutationsList, observer) {
-        console.log('mutation detected')
         // Use traditional 'for loops' for IE 11
         for(let mutation of mutationsList) {
             if (mutation.type === 'childList') {
@@ -15,16 +24,9 @@ window.onload = () => {
                 if(points_button){
                     console.log(points_button)
                     points_button.click()
-                    console.log('button clicked')
                 }
             }
         }
     };
-    
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver(callback);
-    
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
-    
+
 };
