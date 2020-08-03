f0cd16707744e4c957431f916976580089dea43d
TheBadong <gautier.bardon2@gmail.com>
2020-08-03 23:53:40 +0200

    why go complicated huh

--- contentScript.js
+++ contentScript.js
@@ -1,31 +1,8 @@
 window.onload = () => {
-    console.log("Twtich Clicker active !")
-    let targetNode = document.querySelector(".community-points-summary")
-    const config = {attributes: false, childList: true, subtree: true}
-    
-    // Check for already present button
-    if(document.querySelector(".community-points-summary button.tw-button")){
-        document.querySelector(".community-points-summary button.tw-button").click()
-    }
-
-    // Create an observer instance linked to the callback function
-    const observer = new MutationObserver((mutationsList, observer) => {
-        // Use traditional 'for loops' for IE 11
-        for(let mutation of mutationsList) {
-            if (mutation.type === 'childList') {
-
-                let points_button = document.querySelector('.community-points-summary button.tw-button')
-
-                if(points_button){
-                    console.log(points_button)
-                    points_button.click()
-                }
-            }
+    window.setInterval(getPoints, 5000)
+    function getPoints(){
+        if(document.querySelector(".community-points-summary button.tw-button")){
+            document.querySelector(".community-points-summary button.tw-button").click()
         }
-    });
-    
-    // Start observing the target node for configured mutations
-    observer.observe(targetNode, config);
-    
-
+    }
 };
